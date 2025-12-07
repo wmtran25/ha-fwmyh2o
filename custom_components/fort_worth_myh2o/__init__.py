0cce8b5509ff463df8ded6aaf47ae0e18ab83c78
wmtran25 <wmtran25@gmail.com>
2025-12-07 00:43:35 -0600

    Update __init__.py

--- custom_components/fort_worth_myh2o/__init__.py
+++ custom_components/fort_worth_myh2o/__init__.py
@@ -1,36 +1,27 @@
 """
 "Fort Worth MyH2O integration for Home Assistant.
 """
-from __future__ import annotations
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN
-from .coordinator import FWMH2ODataUpdateCoordinator
+DOMAIN = "fort_worth_myh2o"
 
 PLATFORMS = ["sensor"]
 
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    # Initialize storage for integration
     hass.data.setdefault(DOMAIN, {})
-    return True
 
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    coordinator = FWMH2ODataUpdateCoordinator(hass, entry)
-    await coordinator.async_config_entry_first_refresh()
-
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    # Forward the setup to the sensor platform
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
     if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+
     return unload_ok
